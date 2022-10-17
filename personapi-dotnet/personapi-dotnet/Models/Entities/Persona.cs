043efffeb83b711a1b8edb81f2bf4561a8c9f05f
DianaStefaniaGarcia <dstefania.garciay@javeriana.edu.co>
2022-10-16 20:22:52 -0500

    Laboratorio 1
    Procedimiento del laboratorio 1

---
+++ personapi-dotnet/personapi-dotnet/Models/Entities/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace personapi_dotnet.Models.Entities
+{
+    public partial class Persona
+    {
+        public Persona()
+        {
+            Telefonos = new HashSet<Telefono>();
+        }
+
+        public int Cc { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Genero { get; set; } = null!;
+        public int Edad { get; set; }
+
+        public virtual ICollection<Telefono> Telefonos { get; set; }
+    }
+}
