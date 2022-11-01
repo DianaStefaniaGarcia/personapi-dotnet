3ec9332fdc0268339607de9f9c420783303d2903
DianaStefaniaGarcia <dstefania.garciay@javeriana.edu.co>
2022-10-31 20:36:06 -0500

    Revert "Laboratorio2_Villa_Garcia"
    This reverts commit 474cff8e463f4ebfe2c6f36497f2b514efcebb26.

---
+++ personapi-dotnet/personapi-dotnet/Models/Entities/persona_dbContext.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace personapi_dotnet.Models.Entities
+{
+    public partial class persona_dbContext : DbContext
+    {
+        public persona_dbContext()
+        {
+        }
+
+        public persona_dbContext(DbContextOptions<persona_dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudio> Estudios { get; set; } = null!;
+        public virtual DbSet<Persona> Personas { get; set; } = null!;
+        public virtual DbSet<Profesion> Profesions { get; set; } = null!;
+        public virtual DbSet<Telefono> Telefonos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=persona_db;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudio>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("estudios");
+
+                entity.Property(e => e.CcPer).HasColumnName("cc_per");
+
+                entity.Property(e => e.Fecha)
+                    .HasColumnType("date")
+                    .HasColumnName("fecha");
+
+                entity.Property(e => e.IdProf).HasColumnName("id_prof");
+
+                entity.Property(e => e.Univer)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("univer");
+
+                entity.HasOne(d => d.IdProfNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.IdProf)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("estuio_persona_fk");
+
+                entity.HasOne(d => d.IdProf1)
+                    .WithMany()
+                    .HasForeignKey(d => d.IdProf)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("estudio_persona_fk");
+            });
+
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.HasKey(e => e.Cc);
+
+                entity.ToTable("persona");
+
+                entity.Property(e => e.Cc)
+                    .ValueGeneratedNever()
+                    .HasColumnName("cc");
+
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(45)
+                    .IsUnicode(false)
+                    .HasColumnName("apellido");
+
+                entity.Property(e => e.Edad).HasColumnName("edad");
+
+                entity.Property(e => e.Genero)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("genero");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(45)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            modelBuilder.Entity<Profesion>(entity =>
+            {
+                entity.ToTable("profesion");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Des)
+                    .HasColumnType("text")
+                    .HasColumnName("des");
+
+                entity.Property(e => e.Nom)
+                    .HasMaxLength(90)
+                    .IsUnicode(false)
+                    .HasColumnName("nom");
+            });
+
+            modelBuilder.Entity<Telefono>(entity =>
+            {
+                entity.HasKey(e => e.Num);
+
+                entity.ToTable("telefono");
+
+                entity.Property(e => e.Num)
+                    .HasMaxLength(15)
+                    .IsUnicode(false)
+                    .HasColumnName("num");
+
+                entity.Property(e => e.Duenio).HasColumnName("duenio");
+
+                entity.Property(e => e.Oper)
+                    .HasMaxLength(45)
+                    .IsUnicode(false)
+                    .HasColumnName("oper");
+
+                entity.HasOne(d => d.DuenioNavigation)
+                    .WithMany(p => p.Telefonos)
+                    .HasForeignKey(d => d.Duenio)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("telefono_persona_fk");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
